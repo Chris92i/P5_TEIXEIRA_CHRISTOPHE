001c1e469183eb86dd0888c2124a0477e4c1c302
chris92i <chris.p.teixeira@gmail.com>
2021-03-08 23:46:13 +0100

    connexion api

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
+const { response } = require("express");
+
 const apiUrl = "http://localhost:3000/api/teddies";
 
+fetch(apiUrl)
+.then(response => {response.json()}
+.then(teddies) => {console.log(teddies)}
