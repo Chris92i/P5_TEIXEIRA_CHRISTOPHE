ca53d99b1e04ac6ca80f87131227b9b3d5344589
chris92i <chris.p.teixeira@gmail.com>
2021-03-11 11:27:11 +0100

    mise en place de la class ted

--- script.js
+++ script.js
@@ -1,7 +1,51 @@
+let card = document.getElementsById("ted")
+class Teddie {
+    constructor(_id, imgUrl, name, price){
+        this._id = _id;
+        this.imgUrl = imgUrl;
+        this.name = name;
+        this.price = price;
+    }
+    
+}
+
+
 fetch('http://localhost:3000/api/teddies')
 .then(response => response.json())
-.then(teddies => {
-    console.log(teddies);
+.then((teddies) => {
+    for (let teddie in teddies){
+        console.log(teddie);
+        console.log(teddies);
+        console.log(teddies[0]);
+
+        const newElt = ted.createElement("div");
+        let addClass = newElt.div.classList.add("card");
+        card.innerHTML += `
+        <div class="card" style="width: 23%">
+        <img src="" class="card-img-top" alt="">
+        <div class="card-body">
+          <h5 class="card-title"></h5>
+          <p class="card-text"></p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+      </div>
+        `;
+        
+
+
+        card.innerHTML +='
+        <div class="card" style="width: 23%">
+        <img src="" class="card-img-top" alt="">
+        <div class="card-body">
+          <h5 class="card-title"></h5>
+          <p class="card-text"></p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+        '
+    }
+
+
 })
-.catch(error => alert("Erreur : " + error));
 
