53094493fc304996a790a87be01b65d8e49d1ee4
chris92i <chris.p.teixeira@gmail.com>
2021-03-12 11:10:20 +0100

    separation des scripts js

--- script.js
+++ script.js
@@ -1,51 +1,24 @@
-let card = document.getElementsById("ted")
-class Teddie {
-    constructor(_id, imgUrl, name, price){
-        this._id = _id;
-        this.imgUrl = imgUrl;
-        this.name = name;
-        this.price = price;
-    }
-    
-}
-
-
 fetch('http://localhost:3000/api/teddies')
 .then(response => response.json())
 .then((teddies) => {
-    for (let teddie in teddies){
-        console.log(teddie);
-        console.log(teddies);
-        console.log(teddies[0]);
-
-        const newElt = ted.createElement("div");
-        let addClass = newElt.div.classList.add("card");
-        card.innerHTML += `
-        <div class="card" style="width: 23%">
-        <img src="" class="card-img-top" alt="">
-        <div class="card-body">
-          <h5 class="card-title"></h5>
-          <p class="card-text"></p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-      </div>
-      </div>
-        `;
-        
+    for (let index in teddies){
+      console.log(teddies);
+      var ted = new Teddie(
+        teddies[index].colors,
+        teddies[index].description,
+        teddies[index].imageUrl,
+        teddies[index].name,
+        teddies[index].price,
+        teddies[index]._id
+      );
+      let card = ted.getCard();
+      document.getElementById("ted").appendChild(card);
 
 
-        card.innerHTML +='
-        <div class="card" style="width: 23%">
-        <img src="" class="card-img-top" alt="">
-        <div class="card-body">
-          <h5 class="card-title"></h5>
-          <p class="card-text"></p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-      </div>
-        '
     }
 
 
 })
+
+
 
---
+++ teddie.js
@@ -0,0 +1,45 @@
+class Teddie {
+    constructor(colors, description, imageUrl, name, price, _id){
+      this.colors = colors;
+      this.description = description;
+      this.imageUrl = imageUrl;
+      this.name = name;
+      this.price = price;
+      this._id = _id;
+    }
+    getCard(){
+      let card = document.createElement("div");
+      card.classList.add("card", "col-12", "col-md-6");
+      // card.style.width = "23%";
+      
+      let img = document.createElement("img");
+      img.classList.add("card-img-top");
+      img.setAttribute("src", this.imageUrl);
+
+      let cardBody = document.createElement("div");
+      cardBody.classList.add("card-body");
+
+      let cardTitle = document.createElement("h5");
+      cardTitle.classList.add("card-title");
+      cardTitle.textContent = this.name;
+
+      let cardText = document.createElement("p");
+      cardText.classList.add("card-text");
+      cardText.textContent = this.price / 100 + "€";
+
+      let link = document.createElement("a");
+      link.classList.add("btn", "btn-primary");
+      link.setAttribute("href", "./produit.html")
+      link.textContent = "Voir la fiche produit";
+
+      
+      card.appendChild(img);
+      card.appendChild(cardBody);
+      card.appendChild(cardTitle);
+      card.appendChild(cardText);
+      card.appendChild(link);
+
+
+      return card;
+    }
+}
