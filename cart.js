d9b0e43acc2df803e98c634ee786840cda7433bb
chris92i <chris.p.teixeira@gmail.com>
2021-03-30 21:42:26 +0200

    mise ne place des regle panier

---
+++ cart.js
@@ -0,0 +1,25 @@
+class Cart { 
+
+    name: "Obaseki Nosa",
+    name : '',
+    color : '',
+    price : '',
+    qty : ''
+
+
+
+    AddToCart(){
+        if 
+
+
+        localStorage.setItem('cart', JSON.stringify(cart));
+
+    }
+    DelFromCart(){
+        localStorage.removeItem("_id");
+    }
+    DelCart(){
+        localStorage.clear();  
+    }
+
+};
