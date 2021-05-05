b6894273c923b8b1e4100769b656c9c212e84e71
chris92i <chris.p.teixeira@gmail.com>
2021-05-05 11:03:49 +0200

    mise en place tableau confirmation commande

--- order_confirmation.js
+++ order_confirmation.js
@@ -7,24 +7,7 @@ window.onload = () =>{
     console.log(orderId);
     console.log(montantTotal);
 
-    // const clicBtn = document.getElementById("btnAddCart");
-
-
-    // let formOrder = document.getElementById("formOrder");
-    // let firstName = document.getElementById("firstName");
-    // let lastNamed = document.getElementById("lastNamed");
-    // let email = document.getElementById("email");
-    // let address = document.getElementById("address");
-    // let codePostal = document.getElementById("codePostal");
-
-    // formOrder.addEventListener('submit', function(e) {
-    //     e.preventDefault();
-    //     console.log(formOrder.checkValidity);
-    //     if (firstName == "" || lastNamed == "" || email == "" || address == "" || codePostal ==""){
-    //     }
-
-    // });
-
-    
+    document.getElementById("numCommande").innerHTML = orderId;
+    document.getElementById("total").innerHTML = montantTotal + "€";
 
 } 
